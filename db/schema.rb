6b2b83e6012f832b1615a0ddfdbf3b28253a6398
Hannah <lindbladhannah@gmail.com>
2017-01-27 00:35:15 +0100

    Added model for countries

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170126230006) do
+ActiveRecord::Schema.define(version: 20170126232146) do
+
+  create_table "countries", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
