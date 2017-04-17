860eab2f8983b2c57aa9b7a10b2e09c082ecb76d
Hannah <lindbladhannah@gmail.com>
2017-04-17 16:09:37 +0200

    Right format in form according to specifications

--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -2,4 +2,8 @@ class Contestant < ActiveRecord::Base
   belongs_to :contest
 	has_many :placements, inverse_of: :contestant
 	validates :contest_id, presence: true
+
+	def song
+		song = country + ", " + title
+	end
 end
