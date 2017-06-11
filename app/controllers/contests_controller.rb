31692fd619e9dd2c65cd9d6ecb61ea2d8538234c
hannushka <lindbladhannah@gmail.com>
2017-06-11 13:56:00 +0200

    Added authentication to Contest controller
    Users have to log in to be able to access admin panel.

--- app/controllers/contests_controller.rb
+++ app/controllers/contests_controller.rb
@@ -1,2 +1,10 @@
 class ContestsController < ApplicationController
+	before_action :require_login
+
+	def require_login
+    unless logged_in?
+      flash[:danger] = "Du måste logga in för att komma åt denna sidan."
+      redirect_to login_path
+    end
+  end
 end
