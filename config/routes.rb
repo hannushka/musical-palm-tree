5da9fe10e24cace29f86c3fefec6ac7aaf990e26
hannushka <lindbladhannah@gmail.com>
2017-06-11 13:36:17 +0200

    Login and log out for admin users
    Sessions created as well as landing page for successful login.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-	resources :bets
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+	resources :bets, :contests
 	root "static_pages#home"
 end
